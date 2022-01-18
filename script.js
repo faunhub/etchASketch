8b7d737b264d5aab9a0446918896bd16925f4f98
faunhub <76247482+faunhub@users.noreply.github.com>
2022-01-18 21:54:52 +0700

    first day

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('.grid-container');
+// let slider = document.querySelector('.slider');
+// slider.addEventListener('change', changeSize);
+
 let width =16;
-let height =16;
+let height =width;
 for (let i =1; i<=width*height;i++){
     const square = document.createElement('div');
     square.classList.add('grid-item');
     container.appendChild(square);
 }
-
-container.style.gridTemplateColumns = `repeat(${width},40px)`;
+let squareSize = 640/width;
+container.style.gridTemplateColumns = `repeat(${width},${squareSize}px)`;
