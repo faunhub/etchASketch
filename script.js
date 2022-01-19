ae2cb88197d014c20ea731ad5c4f9b586a467aa4
faunhub <76247482+faunhub@users.noreply.github.com>
2022-01-19 22:43:22 +0700

    added color picker

--- script.js
+++ script.js
@@ -8,35 +8,38 @@ function createDrawingPad(size){
     }
     let numberOfColumns = 640/width;
     container.style.gridTemplateColumns = `repeat(${size},${numberOfColumns}px)`;
-    makeDraw();
+    makeDraw("#000000");
 }
 
-function makeDraw(){
-    let pixelCollection = container.childNodes;
+function makeDraw(color){
     pixelCollection.forEach(pixel => pixel.addEventListener('mouseover', 
-    (pixel) => pixel.target.classList.add('grid-item-hover')));
+    (pixel) => pixel.target.style.backgroundColor =color));
 }
 
 function changeSize(){
     container.innerHTML = "";
     sizeDisplay.innerText = slider.value;
+    colorPicker.value = "#000000";
     createDrawingPad(slider.value);
 }
 
 function resetPad(){
     container.innerHTML = "";
+    colorPicker.value = "#000000";
     createDrawingPad(slider.value);
 }
 
-
 const container = document.querySelector('.grid-container');
+const pixelCollection = container.childNodes;
 const slider = document.querySelector('.slider');
 const reset = document.querySelector('.resetButton');
 const sizeDisplay = document.querySelector('.size-display');
+const colorPicker = document.querySelector('.colorPicker');
 
 createDrawingPad(32);
 sizeDisplay.innerText = slider.value;
 slider.addEventListener('change', changeSize);
 reset.addEventListener('click', resetPad);
+colorPicker.addEventListener('change', () => makeDraw(colorPicker.value));
 
 
