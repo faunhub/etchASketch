02162f5ed7ee5bb5d8a1782b274ea5b317455460
faunhub <76247482+faunhub@users.noreply.github.com>
2022-01-19 18:55:12 +0700

    able to draw

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const container = document.querySelector('.grid-container');
-// let slider = document.querySelector('.slider');
+
+function createDrawingPad(size){
+    let width =size;
+    let height =width;
+    for (let i =1; i<=width*height;i++){
+        const square = document.createElement('div');
+        square.classList.add('grid-item');
+        container.appendChild(square);
+    }
+    let squareSize = 640/width;
+    container.style.gridTemplateColumns = `repeat(${width},${squareSize}px)`;
+}
+
+createDrawingPad(64);
+
+let pixelCollection = container.childNodes;
+pixelCollection.forEach(pixel => pixel.addEventListener('mouseover', (pixel) => pixel.target.classList.add('grid-item-hover')));
+
+let slider = document.querySelector('.slider');
 // slider.addEventListener('change', changeSize);
 
-let width =16;
-let height =width;
-for (let i =1; i<=width*height;i++){
-    const square = document.createElement('div');
-    square.classList.add('grid-item');
-    container.appendChild(square);
-}
-let squareSize = 640/width;
-container.style.gridTemplateColumns = `repeat(${width},${squareSize}px)`;
+// function changeSize(){
+//     const size = slider.value;
+//     console.log(size);
+//     createDrawingPad(size);
+// }
